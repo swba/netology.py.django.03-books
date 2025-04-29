710ed5623e920d737d1ca4a227bf2c58458ad959
Stan <stan@kind.co>
2025-04-29 11:00:48 +0300

    Restored debug mode.

--- main/settings.py
+++ main/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oe%64jz$eiok$ote*-^g7r@c*&gi35v3ck3yai$z*kjlh!hjc@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
